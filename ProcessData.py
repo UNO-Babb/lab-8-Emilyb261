9503c0451e5896a066ec92b2c8499a8659e142e3
Emilyb261 <ebillings@unomaha.edu>
2025-11-01 22:12:25 -0500

    Update ProcessData.py

--- ProcessData.py
+++ ProcessData.py
@@ -1,9 +1,7 @@
 #ProcessData.py
-#Name:
+#Name: Emily Billings
 #Date:
 #Assignment:
-
-import random
 
 def main():
 
@@ -12,12 +10,43 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  #line = inFile.readline()
+  for line in inFile: 
+    data = line.split()
+    first = data[0] 
+    last = data[1] 
+    major = data[2] 
+    idNum = data[3] 
+    year = data[4].upper()
 
+    student_id = makeID(first, last, idNum) 
+    
+  
 
+    major_year = major[:3].upper() + "-" + year 
+
+    output = f"{last},{first},{student_id},{major_year}\n"
+    outFile.write(output)
+
+    #print(student_id) 
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+  
+def makeID(first, last, idNum):
+  #print(first, last, idNum) 
+  IdLen = len(idNum) 
+
+  while len(last) < 5:
+    last = last + "X" 
+
+  id = first[0] + last + idNum[IdLen - 3: ] 
+
+  #print(id) 
+  return id 
+
+
 
 if __name__ == '__main__':
   main()
